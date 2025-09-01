c3ba6a27057196cebdf9b971f66721b38eb42db3
bocayuva <frs.rodrigobocayuva@gmail.com>
2025-09-01 20:01:19 -0300

    sobre

--- app/hello/routes.py
+++ app/hello/routes.py
@@ -1,7 +1,12 @@
 from flask import Blueprint
 
 hello_bp = Blueprint('hello',__name__)
+hello_name = Blueprint('hello_name',__name__)
 
 @hello_bp.route('/')
 def index():
-    return 'Hello World'
+    return 'Olá, Mundo!'
+
+@hello_name.route('/sobre')
+def index():
+    return 'Olá, Rodrigo!'
